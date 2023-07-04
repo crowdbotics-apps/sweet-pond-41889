aa2e9b32cc5ea32a94f94adbd69b061f3ea02a44
Crowdbotics <team@crowdbotics.com>
2023-07-04 01:47:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-pond-41889",
+  url: "https://sweet-pond-41889.botics.co",
+  api: "https://sweet-pond-41889.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
